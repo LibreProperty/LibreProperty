8371ab306fe02356bf0aae078a3b8c1bbc8aace6
Sam Stoelinga <sammiestoel@gmail.com>
2022-12-17 09:42:32 -0800

    Add landing page

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
