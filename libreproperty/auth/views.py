51aa8e350f45200fc66bdffc4012078e65fdb7ef
Sam Stoelinga <sammiestoel@gmail.com>
2022-12-19 17:08:38 -0800

    add login

--- libreproperty/auth/views.py
+++ libreproperty/auth/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
-from flask_login import login_user
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user
 
 from libreproperty import db
 from .forms import SignupForm, LoginForm
@@ -29,4 +29,17 @@ def signup():
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html', form=LoginForm())
+    if current_user.is_authenticated:
+        return redirect(url_for('pages_bp.index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(password=form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page or url_for('pages_bp.index'))
+        flash('Invalid username/password combination')
+        return redirect(url_for('auth_bp.login'))
+
+    return render_template('auth/login.html', form=form)
