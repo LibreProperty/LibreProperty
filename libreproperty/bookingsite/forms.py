8161255cec69d7643948c3df702c2338d0df4c9e
Sam Stoelinga <sammiestoel@gmail.com>
2023-01-30 11:35:53 -0800

    add error handling for booking form

--- libreproperty/bookingsite/forms.py
+++ libreproperty/bookingsite/forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
 
-from wtforms import DateField, IntegerField, StringField, EmailField
+from wtforms import DateField, IntegerField, StringField, EmailField, TextAreaField
+from wtforms.validators import DataRequired
 
 
 class BookingForm(FlaskForm):
-    checkin = DateField()
-    checkout = DateField()
-    guests = IntegerField()
-    first_name = StringField()
-    last_name = StringField()
-    email = EmailField()
-    phone = StringField()
+    checkin = DateField(format="%m/%d/%Y", validators=[DataRequired("Please tell us when you're checking in")])
+    checkout = DateField(format="%m/%d/%Y", validators=[DataRequired("Please tell us when you're checking out")])
+    guests = IntegerField(validators=[DataRequired()])
+    first_name = StringField(validators=[DataRequired("Please provide your first name")])
+    last_name = StringField(validators=[DataRequired("Please provide your last name")])
+    email = EmailField(validators=[DataRequired("Please provide a valid email address")])
+    phone = StringField(validators=[DataRequired("Please provide a phone number")])
+    comments = TextAreaField(validators=[DataRequired("Please provide a short introduction and reason for your stay")])
